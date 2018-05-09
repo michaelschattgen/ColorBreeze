2f60d2ef3bfdfc15fbd1727f617d33252d163142
Michael Schättgen <michael@schattgen.me>
2018-05-09 13:49:28 +0200

    Started working on basic permission check

--- app/src/main/java/com/reverp/colorbreeze/MainActivity.java
+++ app/src/main/java/com/reverp/colorbreeze/MainActivity.java
@@ -1,7 +1,11 @@
 package com.reverp.colorbreeze;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_SECURE_SETTINGS);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(this, "Success!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "nope :(", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
