4cfb2f8a2e2a4c24580ae8381b6fa1fd4a235988
Michael Schättgen <michael@schattgen.me>
2018-05-15 00:22:30 +0200

    Fixed EnableGrayscaleService by using persistent notification

--- app/src/main/java/com/reverp/colorbreeze/EnableGrayscaleService.java
+++ app/src/main/java/com/reverp/colorbreeze/EnableGrayscaleService.java
@@ -1,12 +1,23 @@
 package com.reverp.colorbreeze;
 
+import android.app.IntentService;
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.JobIntentService;
+import android.support.v4.app.NotificationCompat;
 
-public class EnableGrayscaleService extends Service {
+public class EnableGrayscaleService extends JobIntentService {
+
+    public EnableGrayscaleService() {
+        super();
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -14,11 +25,32 @@ public class EnableGrayscaleService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public void onCreate() {
+        //The intent to launch when the user clicks the expanded notification
+        Intent intent = new Intent(this, SomeActivityToLaunch.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
+        Notification.Builder builder = new Notification.Builder(this)
+                .setContentTitle("Grayscale mode activated")
+                .setContentText("Press here to deactivate and bring back your colors.")
+                .setSmallIcon(R.drawable.ic_ink)
+                .setActions()
+                .setOngoing(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId("channel1");
+        }
+
+        startForeground(1234, builder.build());
         Settings.Secure.putInt(getContentResolver(), "accessibility_display_daltonizer", 0);
         Settings.Secure.putInt(getContentResolver(), "accessibility_display_daltonizer_enabled", 1);
 
-        return super.onStartCommand(intent, flags, startId);
+        super.onCreate();
+    }
+
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+
     }
 }
