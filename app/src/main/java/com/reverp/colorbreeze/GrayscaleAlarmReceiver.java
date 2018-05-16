3bc0481ce1a62fff3c0543d25002953fdbfb79ed
Michael Schättgen <michael@schattgen.me>
2018-05-16 09:36:38 +0200

    Started working on day preference

--- app/src/main/java/com/reverp/colorbreeze/GrayscaleAlarmReceiver.java
+++ app/src/main/java/com/reverp/colorbreeze/GrayscaleAlarmReceiver.java
@@ -3,15 +3,32 @@ package com.reverp.colorbreeze;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Set;
 
 public class GrayscaleAlarmReceiver extends BroadcastReceiver {
     public static final String ENABLE_GRAYSCALE_CODE = "ENABLE_GRAYSCALE";
     public static final String DISABLE_GRAYSCALE_CODE = "DISABLE_GRAYSCALE";
 
+    private Context _context;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        this._context = context;
+
+        if(!IsSelectedDay())
+        {
+            return;
+        }
+
         Intent serviceIntent = null;
         String actionString = intent.getAction();
 
@@ -32,5 +49,19 @@ public class GrayscaleAlarmReceiver extends BroadcastReceiver {
             serviceIntent = new Intent(context, EnableGrayscaleService.class);
             context.stopService(serviceIntent);
         }
+    }
+
+    private boolean IsSelectedDay()
+    {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this._context);
+        Set<String> selectedDays = prefs.getStringSet("pref_selected_days", null);
+
+        String todayString = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(Calendar.getInstance().getTime());
+        if(!selectedDays.isEmpty())
+        {
+            return selectedDays.contains(todayString);
+        }
+
+        return false;
     }
 }
